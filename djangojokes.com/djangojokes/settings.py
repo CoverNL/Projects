fc45ef285637b024f6e23e0d88bfaeb4e60f0576
CoverNL <yannickkok1@gmail.com>
2024-12-13 10:51:42 +0100

    pythonAnywhere start

--- djangojokes.com/djangojokes/settings.py
+++ djangojokes.com/djangojokes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9-amcbl3og@%p2&kbnp_d+$55%)=j4290-=)ihooz-2=qa++u&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yannickkok.pythonanywhere.com"]
 
 
 # Application definition
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
